e0158d8a51d87effe261e995904bffad3060b937
bellDev-code <sumaoo20@naver.com>
2021-07-25 18:23:35 +0900

    bctypt password

--- back/src/routers/users.js
+++ back/src/routers/users.js
@@ -2,12 +2,11 @@ const router = require("express").Router();
 const db = require("../db");
 const dayjs = require("dayjs");
 const { emailRegex } = require("../utils/regex.js");
+const bcrypt = require("bcrypt");
 
 // JOIN USER
 router.post("/", async (req, res, next) => {
   console.log(req.body);
-
-  const client = await db.connect();
 
   const now = dayjs().format();
 
@@ -15,12 +14,18 @@ router.post("/", async (req, res, next) => {
     if (!emailRegex.test(req.body.email)) {
       throw new Error("email 형식이 올바르지 않습니다. 확인해주세요.");
     }
+
+    const salt = await bcrypt.genSalt(+process.env.PASSWORD_ROUND_LENGTH);
+    const hashed = await bcrypt.hash(req.body.password, salt);
+
+    const client = await db.connect();
+
     await client.query(
       `
         INSERT INTO public.User (username, password, name, email, createdAt)
         VALUES ($1, $2, $3, $4, $5)
       `,
-      [req.body.username, req.body.password, req.body.name, req.body.email, now]
+      [req.body.username, hashed, req.body.name, req.body.email, now]
     );
     client.release();
 
@@ -85,7 +90,38 @@ router.get("/:id", async (req, res, next) => {
 
 // LOGIN USER
 router.post("/login", async (req, res, next) => {
-  res.send("Login User!");
+  try {
+    const { username, password } = req.body;
+
+    const client = await db.connect();
+
+    const result = await client.query(
+      `
+        SELECT username, password FROM public.User WHERE username = $1
+      `,
+      [username]
+    );
+
+    if (result.rows.length < 1) {
+      console.log("존재하지 않는 계정입니다.");
+    }
+
+    client.release();
+
+    const user = result.rows[0];
+
+    const compared = await bcrypt.compare(password, user.password);
+    console.log(compared);
+
+    if (!compared) {
+      throw Error("존재하지 않는 계정입니다.");
+    }
+
+    return res.send("ok");
+  } catch (error) {
+    console.error(error);
+    return res.status(403).send(error.message);
+  }
 });
 
 // Delete USER
