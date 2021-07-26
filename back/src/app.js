3e4be4a5c70ac12c4312c0319f2f21399e7e465c
bellDev-code <sumaoo20@naver.com>
2021-07-26 15:24:55 +0900

    code review

--- back/src/app.js
+++ back/src/app.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const db = require("./db");
 
-// db connecting test
-// const test = async () => {
-//   db.connect();
-//   db.query("SELECT NOW()", (err, res) => {
-//     console.log(err, res);
-//     db.end();
-//   });
-// };
-
-// test();
 const app = express();
 app.use(express.json());
 
@@ -21,13 +11,6 @@ app.set("PORT", process.env.PORT || 4190);
 const api = require("./routers");
 
 app.use("/api", api);
-
-// router 써서 필요 없어짐
-// app.get("*", (req, res, next) => {
-//   console.log("hello world!");
-
-//   res.send("ok");
-// });
 
 app.listen(app.get("PORT"), () => {
   console.log(`listen on localhost:${app.get("PORT")}`);
--- back/src/routers/users.js
+++ back/src/routers/users.js
@@ -6,15 +6,15 @@ const bcrypt = require("bcrypt");
 
 // JOIN USER
 router.post("/", async (req, res, next) => {
-  console.log(req.body);
-
   const now = dayjs().format();
 
   try {
+    // email check
     if (!emailRegex.test(req.body.email)) {
       throw new Error("email 형식이 올바르지 않습니다. 확인해주세요.");
     }
 
+    // use bcrypt
     const salt = await bcrypt.genSalt(+process.env.PASSWORD_ROUND_LENGTH);
     const hashed = await bcrypt.hash(req.body.password, salt);
 
@@ -63,31 +63,6 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-// params 예제
-// router.get("/:username/products/:brandname", async (req, res, next) => {
-//   try {
-//     const client = await db.connect();
-//     console.log(req.params);
-
-//     return;
-//     const { id } = req.params;
-
-//     const result = await client.query(
-//       `
-//         SELECT * FROM public.User WHERE id = $1
-//       `,
-//       [id]
-//     );
-
-//     console.log(result);
-
-//     client.release();
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(403).send(error.message);
-//   }
-// });
-
 // LOGIN USER
 router.post("/login", async (req, res, next) => {
   try {
@@ -105,13 +80,12 @@ router.post("/login", async (req, res, next) => {
     if (result.rows.length < 1) {
       console.log("존재하지 않는 계정입니다.");
     }
-
     client.release();
 
     const user = result.rows[0];
 
     const compared = await bcrypt.compare(password, user.password);
-    console.log(compared);
+    // console.log(compared);
 
     if (!compared) {
       throw Error("존재하지 않는 계정입니다.");
