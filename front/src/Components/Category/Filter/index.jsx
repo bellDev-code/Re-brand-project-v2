46def9b72e5ca169da66e223b01d26aa8d88bacd
bellDev-code <sumaoo20@naver.com>
2021-10-15 22:25:01 +0900

    Constitute Filter Component

--- front/src/Components/Category/Filter/index.jsx
+++ front/src/Components/Category/Filter/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Filter = () => {
-  return <div>필터</div>;
+  return (
+    <div>
+      <div>
+        <div>가방</div>
+        <div>시계</div>
+      </div>
+      <div>브랜드</div>
+      <div>성별</div>
+    </div>
+  );
 };
 
 export default Filter;
