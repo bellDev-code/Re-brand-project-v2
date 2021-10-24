51fdb8adfb32319ba121168a7e225ef9f5e2ae49
hoonn9 <hun9935@gmail.com>
2021-10-24 18:23:34 +0900

    Move product detail query to SQL source

--- back/src/db/sql.js
+++ back/src/db/sql.js
@@ -13,6 +13,26 @@ const sql = {
   verify: {
     find: 'SELECT * FROM public."Verification" WHERE payload = $1 AND code = $2',
   },
+  product: {
+    findDetail: `
+    SELECT 
+    p.id, p.name, p.price, p.count, p.sale, p."categoryId", p."createdAt", p."updatedAt",
+    pi.id AS info_id, 
+    pi.color AS info_color, 
+    pi."offerGender" AS "info_offerGender", 
+    pi.size AS info_size, 
+    pi.manufacturer AS info_manufacturer, 
+    pi.origin AS info_origin, 
+    pd.id AS detail_id, 
+    pd.material AS detail_material,
+    b.id AS brand_id,
+    b.name AS brand_name
+    FROM public."Product" AS p 
+    INNER JOIN "ProductInfo" pi ON p."infoId" = pi.id
+    INNER JOIN "ProductDetail" pd ON p."detailId" = pd.id
+    INNER JOIN "Brand" b ON p."brandId" = b.id
+  `,
+  },
 };
 
 module.exports = sql;
